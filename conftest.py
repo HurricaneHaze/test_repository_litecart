54c4bbc329ccc2db5210e162541f62249847c8dc
Кудрявцев Юрий <phlatline04@gmail.com>
2021-02-24 21:36:50 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,7 +4,21 @@ from fixture.application import Application
 
 
 @pytest.fixture()
-def app(request):
+def admin_console_app(request):
     fixture = Application()
+    fixture.admin_console_session.login_admin_console(username='admin', password='admin')
+
+    def fin():
+        fixture.admin_console_session.logout_admin_console()
+        fixture.destroy()
+
+    request.addfinalizer(fin)
+    return fixture
+
+
+@pytest.fixture()
+def litecart_app(request):
+    fixture = Application()
+    fixture.open_home_page()
     request.addfinalizer(fixture.destroy)
     return fixture
