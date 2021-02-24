c0133779ff0ed782f253187fabfe31a1f1f89b1d
Кудрявцев Юрий <phlatline04@gmail.com>
2021-02-24 21:56:14 +0300

    update test check duck sticker

--- test/test_admin_console/test_admin_console.py
+++ test/test_admin_console/test_admin_console.py
@@ -6,27 +6,38 @@ def test_menu_bar(admin_console_app):
     admin_console_app.admin_console_menu.select_appearance()
     admin_console_app.admin_console_menu.open_template_page()
     assert admin_console_app.admin_console_menu.get_page_header()
+
     admin_console_app.admin_console_menu.open_logotype_page()
     assert admin_console_app.admin_console_menu.get_page_header()
+
     admin_console_app.admin_console_menu.select_catalog()
     admin_console_app.admin_console_menu.open_catalog_page()
     assert admin_console_app.admin_console_menu.get_page_header()
+
     admin_console_app.admin_console_menu.open_product_groups_page()
     assert admin_console_app.admin_console_menu.get_page_header()
+
     admin_console_app.admin_console_menu.open_option_groups_page()
     assert admin_console_app.admin_console_menu.get_page_header()
+
     admin_console_app.admin_console_menu.open_manufacturers_page()
     assert admin_console_app.admin_console_menu.get_page_header()
+
     admin_console_app.admin_console_menu.open_suppliers_page()
     assert admin_console_app.admin_console_menu.get_page_header()
+
     admin_console_app.admin_console_menu.open_delivery_statuses_page()
     assert admin_console_app.admin_console_menu.get_page_header()
+
     admin_console_app.admin_console_menu.open_sold_out_statuses_page()
     assert admin_console_app.admin_console_menu.get_page_header()
+
     admin_console_app.admin_console_menu.open_quantity_units_page()
     assert admin_console_app.admin_console_menu.get_page_header()
+
     admin_console_app.admin_console_menu.open_csv_page()
     assert admin_console_app.admin_console_menu.get_page_header()
+
     admin_console_app.admin_console_menu.open_customers_page()
     assert admin_console_app.admin_console_menu.get_page_header()
 
--- test/test_litecart/test_litecart.py
+++ test/test_litecart/test_litecart.py
@@ -3,4 +3,10 @@ import pytest
 
 
 def test_stickers(litecart_app):
-    assert litecart_app.home_page.check_sticker(litecart_app.home_page.find_purple_duck_in_most_popular())
+    check_duck_sticker = litecart_app.home_page.check_sticker
+    assert check_duck_sticker(litecart_app.home_page.find_purple_duck_in_most_popular())
+    assert check_duck_sticker(litecart_app.home_page.find_red_duck_in_most_popular())
+    assert check_duck_sticker(litecart_app.home_page.find_green_duck_in_most_popular())
+    assert check_duck_sticker(litecart_app.home_page.find_blue_duck_in_most_popular())
+    assert check_duck_sticker(litecart_app.home_page.find_yellow_duck_in_most_popular())
+
