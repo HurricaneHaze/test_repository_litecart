1eaccaf28d6b3e216253f3439ac1e7564fb939ee
Кудрявцев Юрий <phlatline04@gmail.com>
2021-02-28 21:11:04 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,23 +2,32 @@
 import pytest
 from fixture.application import Application
 
+fixture = None
+
 
 @pytest.fixture()
 def ad_app(request):
-    fixture = Application()
-    fixture.ad_session.login_admin_console(username='admin', password='admin')
-
-    def fin():
-        fixture.ad_session.logout_admin_console()
-        fixture.destroy()
-
-    request.addfinalizer(fin)
+    global fixture
+    if fixture is None or not fixture.is_valid():
+        fixture = Application()
+        fixture.ad_session.login_admin_console(username='admin', password='admin')
     return fixture
 
 
 @pytest.fixture()
 def app(request):
-    fixture = Application()
+    global fixture
+    if fixture is None or not fixture.is_valid():
+        fixture = Application()
     fixture.open_home_page()
-    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        # fixture.ad_session.logout_admin_console()
+        fixture.destroy()
+
+    request.addfinalizer(fin)
     return fixture
